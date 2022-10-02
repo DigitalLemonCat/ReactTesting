045a657997640700755a2956d6f6c27fb5abbc9c
Aga <a.skaczukowska@gmail.com>
2022-10-02 19:36:19 +0200

    second commit

---
+++ src/components/greet/dist/greet.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.Greet = void 0;
+exports.Greet = function (props) {
+    return (React.createElement("div", null,
+        "Hello ",
+        props.name));
+};
+//10, 1:05 
---
+++ src/components/greet/greet.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react'
+import { Greet } from './greet'
+
+
+/**
+ * Greet should render the text hello and if a name is passed into the component 
+ * it should render hello followed by the name
+ */
+
+test('Greet renders correctly', () => {
+    render(<Greet />)
+    const textElement = screen.getByText('Hello')
+    expect(textElement).toBeInTheDocument()
+})
+ 
+test('Greet renders with a name', () => {
+    render(<Greet name='Hannah' />)
+    const textElement = screen.getByText('Hello Hannah')
+    expect(textElement).toBeInTheDocument()
+}) 
+
